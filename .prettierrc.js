3db0aee82bfeddc7e9fd8b05034a9db3e949f675
xi.chen1 <xi.chen@1111.163.com>
2025-08-13 21:54:08 +0800

    升级到taro4

---
+++ .prettierrc.js
@@ -0,0 +1,26 @@
+export default {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  overrides: [
+    {
+      files: '*.{ts,tsx}',
+      options: {
+        parser: 'typescript',
+      },
+    },
+    {
+      files: '*.{scss,sass}',
+      options: {
+        parser: 'scss',
+      },
+    },
+  ],
+}
